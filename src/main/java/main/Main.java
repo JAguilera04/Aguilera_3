98d559158066ab5d0569e307d8b939a74e0b2639
hecnauj <48989584+hecnauj@users.noreply.github.com>
2019-09-08 23:27:59 -0300

    sin test

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+import datos.Pedido;
+/**
+ *
+ * @author usuario
+ */
+public class Main {
+    public static void main (String [] args) {
+        Pedido p = new Pedido();
+        p.menu();
+        }
+    
+}
